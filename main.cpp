1dec60cdb22117d4ad0e8ca07ea50fc1d524bd59
Coates, Dakari <dakman5104@gmail.com>
2026-02-02 02:12:10 +0000

    works

--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main() {
   const double length = 7.2;
   const double width = 1.3;
-
-
+ double area = length * width;
+ double perimiter = 2 * (length + width);
+ cout << "rectangle Properties : " << endl;
+ cout << "length = " << length << endl;
+ cout <<  "width = " << width << endl;
+ cout << "area = " << area << endl; 
+cout << " perimiter = " << perimiter << endl;
 }
